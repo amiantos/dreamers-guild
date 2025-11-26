f20f092ec20b24c74de0d415f7d5b5d32ef5cfd3
Brad Root <bradroot@me.com>
2025-11-25 21:33:06 -0800

    add basepath more places for demo mode build

--- vue_client/src/main.js
+++ vue_client/src/main.js
@@ -8,7 +8,7 @@ import WorkersView from './views/WorkersView.vue'
 import './assets/colors.css'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
