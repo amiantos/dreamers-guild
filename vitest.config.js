a5e096273edbd51540eb215a78c5f0b17c1086e4
Brad Root <bradroot@me.com>
2025-12-04 14:09:08 -0800

    try to add test coverage reports

--- vitest.config.js
+++ vitest.config.js
@@ -6,5 +6,11 @@ export default defineConfig({
     environment: 'node',
     include: ['server/**/*.test.js'],
     testTimeout: 10000,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      include: ['server/**/*.js'],
+      exclude: ['server/tests/**'],
+    },
   },
 });
