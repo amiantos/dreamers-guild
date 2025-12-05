8bb35757b7548a82ebaa5b652d65fbad7a5dae61
Brad Root <bradroot@me.com>
2025-12-04 22:08:03 -0800

    update default prompts to use better styles

--- vue_client/src/config/presets.js
+++ vue_client/src/config/presets.js
@@ -3,63 +3,63 @@
 export const samplePrompts = [
   {
     prompt: "A post-apocalyptic wasteland with an amusement park in ruins in the style of vintage poster, featuring distressed textures, muted colors, and bold typography, reminiscent of mid-century advertisements or travel posters. The poster says \"WELCOME!!!\"",
-    style: "flux"
+    style: "flux-landscape"
   },
   {
     prompt: "Schematics for a steampunk computer from the Victorian Age, showcasing long lost technology. ink and watercolor on parchment",
-    style: "albedo3.1"
+    style: "sdxl-landscape"
   },
   {
     prompt: "A penguin dressed like a superhero, wearing a red cape and flying through the air. Cartoon comic illustration in the style of roy lichtenstein and steve ditko, centered on a white background",
-    style: "deliberate"
+    style: "sdxl-portrait"
   },
   {
     prompt: "Industrial urban scene featuring stark, geometric buildings lining a narrow, reflective canal. Power lines stretch overhead, enhancing the sense of depth in this minimalist black and white illustration.",
-    style: "flux"
+    style: "flux-landscape"
   },
   {
     prompt: "A refined watercolor portrait of a distinguished penguin in a tailored suit and aviator sunglasses, looking heroic. Vibrant colors, artistic brushstrokes",
-    style: "flux"
+    style: "flux-portrait"
   },
   {
     prompt: "Himalayan mountains, flat design, vibrant colors, Moebius",
-    style: "deliberate"
+    style: "sdxl-landscape"
   },
   {
     prompt: "An astronaut resting on Mars in a beach chair.",
-    style: "albedo3.1"
+    style: "sdxl-landscape"
   },
   {
     prompt: "Mountain chalet covered in snow, foggy, sunrise, sharp details, sharp focus, elegant, highly detailed, illustration, by Jordan Grimmer and Greg Rutkowski",
-    style: "albedo3.1"
+    style: "sdxl-landscape"
   },
   {
     prompt: "Graffiti-style picture of a Raven, alcohol markers and aerosol paint",
-    style: "albedo3.1"
+    style: "sdxl-portrait"
   },
   {
     prompt: "macro photograph of a brisket on a table with beer, in a blurred restaurant with depth of field, bokeh, soft diffused light, professional food photography",
-    style: "deliberate"
+    style: "sdxl-landscape"
   },
   {
     prompt: "Beautiful portrait oil painting of an aristocrat chipmunk",
-    style: "albedo3.1"
+    style: "sdxl-portrait"
   },
   {
     prompt: "San Francisco Downtown, sunset, flat design poster, minimalist, modern, 4k, epic composition, flat vector art illustration, stunning realism, long shot, unreal engine 4d",
-    style: "albedo3.1"
+    style: "sdxl-portrait"
   },
   {
     prompt: "Cartoon animation style a cool penguin wearing sunglasses, surfing on a wave. The penguin has a playful expression, standing confidently on a surfboard, with one flipper raised in a thumbs-up gesture. The wave is a vibrant blue with white frothy details, curling dynamically around the penguin. The background includes a sunny sky with a few fluffy clouds. The overall style is bright, colorful, and cheerful, typical of classic Disney animation.",
-    style: "albedo3.1"
+    style: "sdxl-widescreen"
   },
   {
     prompt: "Plans for a mechanical brain, drawn in the style of Leonardo Da Vinci",
-    style: "albedo3.1"
+    style: "sdxl-widescreen"
   },
   {
     prompt: "Movie poster that says \"DREAMERS GUILD\" in the style of a 1980s fantasy adventure movie",
-    style: "cascade+"
+    style: "flux-portrait"
   }
 ]
 
